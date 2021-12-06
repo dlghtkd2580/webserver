7d1c53b1a3e074e3decd55b4b1c111ac7c6984b6
dlghtkd2580 <dlghtkd2580@gmail.com>
2021-12-06 18:38:44 +0900

    ver_1

---
+++ web/models.py
@@ -0,0 +1,101 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+GENDER_CHOICES = {
+    ('man', '남자'),
+    ('woman', '여자'),
+}
+
+ANSWER_CHOICES = {
+    ('yes', '예'),
+    ('no', '아니오'),
+}
+
+HOUSE_CHOICES = {
+    ('charter', '전세'),
+    ('monthly', '월세'),
+    ('hold', '보유'),
+}
+
+DATE_CHOICES = {
+    ('daily', '일수'),
+    ('monthly', '월상환'),
+    ('weekly', '주상환'),
+}
+
+
+
+class Form(models.Model):
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    name = models.CharField(max_length=40, verbose_name="이름")
+
+    phone_number = models.CharField(max_length=40, verbose_name="휴대폰번호")
+
+    region = models.CharField(max_length=400, verbose_name="지역")
+
+    age = models.IntegerField(validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ], verbose_name="나이")
+
+    gender = models.CharField(max_length=40, choices=GENDER_CHOICES, verbose_name="성별")
+
+    job = models.CharField(null=True, blank=True, max_length=400, verbose_name="직업")
+
+    pay = models.IntegerField(validators=[
+            MaxValueValidator(100000000),
+            MinValueValidator(0)
+        ], verbose_name="급여(만원)")
+
+    pay_day = models.IntegerField(validators=[
+            MaxValueValidator(31),
+            MinValueValidator(1)
+        ], verbose_name="급여날짜(일)")
+
+    use_bool = models.CharField(max_length=40, choices=ANSWER_CHOICES, verbose_name="개인돈사용여부")
+    use_ps = models.TextField(max_length=4000, null=True, blank=True, verbose_name="개인돈사용내역")
+
+    credit_grade = models.IntegerField(validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ], verbose_name="신용등급")
+
+    holding_credit = models.IntegerField(null=True, blank=True,validators=[
+            MaxValueValidator(1000000),
+            MinValueValidator(0)
+        ], verbose_name="기대출(만원)")
+
+    holding_house = models.CharField(max_length=40, choices=HOUSE_CHOICES, verbose_name="자가보유")
+
+    holding_car = models.CharField(max_length=40, choices=ANSWER_CHOICES, verbose_name="자차보유")
+
+    credit_amount = models.IntegerField(validators=[
+            MaxValueValidator(100000000),
+            MinValueValidator(1)
+        ], verbose_name="대출신청금(만원)")
+
+    credit_date = models.CharField(null=True,blank=True,max_length=40, choices=DATE_CHOICES, verbose_name="원하는상환방식")
+
+
+    def __str__(self):
+        return '(' + str(self.pk) + ') ' + str(self.date) + ' / ' +self.name + ' / ' + str(self.age) + '세 / ' + self.region
+
+    class Meta:
+        db_table = 'forms'
+        verbose_name = '신청서'
+        verbose_name_plural = '신청서'
+
+class User(models.Model):
+    user_id = models.CharField(unique=True, max_length=50, verbose_name="아이디")
+    user_pw = models.CharField(max_length=200, verbose_name="비밀번호")
+
+    def __str__(self):
+        return str(self.user_id + ' / ' + self.user_pw)
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = '유저'
+        verbose_name_plural = '유저'
---
+++ web/static/web/js/validate.js
@@ -0,0 +1,82 @@
+function validate_form() {
+       var re1 = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{1,10}$/;
+       var re2 = /^[0-9]{1,4}$/;
+       var re3 = /^[0-9]{1,3}$/;
+       var re4 = /^[0-9]{1,9}$/;
+       var re5 = /^[0-9]{1,2}$/;
+       var re6 = /^[0-9]{1,2}$/;
+       var re7 = /^[0-9]{1,9}$/;
+
+
+       var re = /^[a-zA-Z0-9]{4,20}$/;
+       var re = /^.{1,100}$/;
+       var re = /^.{1,5000}$/;
+
+       var name = document.getElementById("name");
+       var phone_number_1 = document.getElementById("phone_number_1");
+       var phone_number_2 = document.getElementById("phone_number_2");
+       var phone_number_3 = document.getElementById("phone_number_3");
+       var age = document.getElementById("age");
+       var pay = document.getElementById("pay");
+       var pay_day = document.getElementById("pay_day");
+       var credit_grade = document.getElementById("credit_grade");
+       var credit_amount = document.getElementById("credit_amount");
+
+       if(!check(re1,name,"신청자 이름을 제대로 입력해주세요.")) {
+           return false;
+       }
+       if(!check(re2,phone_number_1,"전화번호 첫번쨰칸을 제대로 입력해주세요.")) {
+           return false;
+       }
+       if(!check(re2,phone_number_2,"전화번호 두번째칸을 제대로 입력해주세요.")) {
+           return false;
+       }
+       if(!check(re2,phone_number_3,"전화번호를 세번째칸을 입력해주세요.")) {
+           return false;
+       }
+       if(!check(re3,age,"나이를 제대로 입력해주세요.")) {
+           return false;
+       }
+       if(!check(re4,pay,"월수입을 제대로 입력해주세요.\n숫자만 입력가능합니다. (단위: 만원)")) {
+           return false;
+       }
+       if(!check(re5,pay_day,"급여일을 제대로 입력해주세요.\n1~31 까지의 숫자만 입력가능합니다.")) {
+           return false;
+       }
+       if(!check(re6,credit_grade,"신용등급을 제대로 입력해주세요.\n1~10 까지의 숫자만 입력가능합니다.")) {
+           return false;
+       }
+       if(!check(re7,credit_amount,"대출신청금액을 제대로 입력해주세요.\n숫자만 입력가능합니다. (단위: 만원)")) {
+           return false;
+       }
+
+       alret("해당업체의 심사 신청이 완료되었습니다.");
+   }
+
+
+function validate_login() {
+
+        var re1 = /^[a-zA-Z0-9]{1,20}$/;
+        var re2 = /^.{1,20}$/;;
+
+        var user_id = document.getElementById("user_id");
+        var user_pw = document.getElementById("user_pw");
+
+        if(!check(re1, user_id, "아이디는 영문 또는 숫자 1~20자 이내 입니다.")) {
+            return false;
+        }
+
+        if(!check(re2, user_pw, "비밀번호는 1~20자 이내 입니다.")) {
+            return false;
+        }
+
+    }
+
+function check(re, what, message) {
+    if(re.test(what.value)) {
+        return true;
+    }
+    alert(message);
+    what.value = "";
+    what.focus();
+}
